4013344580ce698a11aea816e607a664532bc0aa
AJBotsss <162437079+AJBotsss@users.noreply.github.com>
2024-03-14 10:06:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Botssusers:Botssusers@cluster0.ce8gwbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Bot:<password>@cluster0.nktowpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
